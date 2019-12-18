35f52a87a18abe2e546826b8297d7f6c2f86d199
Phil Barker <phil.barker@pjjk.co.uk>
2019-12-18 17:23:01 +0000

    adding test against expected results of test file

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,5 +7,12 @@ pp = pprint.PrettyPrinter(indent=4)  # used for debugging
 def test_integration(test_client):
     url = "https://pjjk.net/ldchecker/demo.html"
     response = test_client.get("/check?url=" + url)
-    pp.pprint(response.data)
-    assert False
+    # uncomment this block to write new expected results if you
+    # change any checks or the file tested
+    #    with open('tests/output/response.html', 'w+b') as o_file:
+    #        o_file.write(response.data)
+    #        o_file.close
+    #    expected_response = open('tests/output/response.html', 'r+b')
+    # the order of the test results in the report is not fixed
+    for line in expected_response.readline():
+        assert line in response.data
